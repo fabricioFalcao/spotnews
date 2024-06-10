b919ecb8f5e014a291b065ebc7fe11bb600e4eff
fabricioFalcao <fabriciohfd@gmail.com>
2024-06-10 16:38:14 -0300

    Requisit 5

--- news/views.py
+++ news/views.py
@@ -3,5 +3,9 @@ from news.models import News
 
 # Create your views here.
 def home(request):
-    context = {"news": News.objects.all()}
+    context = {"all_news": News.objects.all()}
     return render(request, 'home.html', context)
+
+def news_details(request, id):
+    context = {"news": News.objects.get(id=id)}
+    return render(request, 'news_details.html', context)
