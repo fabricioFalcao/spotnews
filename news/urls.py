762cadd581ef3b3cf862cd306fd5ccc3d2f5bf7c
fabricioFalcao <fabriciohfd@gmail.com>
2024-06-10 22:14:53 -0300

    Requisit 6

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from news.views import home, news_details
+from news.views import home, news_details, categories
 
 urlpatterns = [
     path("", home, name="home-page"),
-    path("news/<int:id>", news_details, name="news-details-page")
+    path("news/<int:id>", news_details, name="news-details-page"),
+    path("categories", categories, name="categories-form"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from news.models import News
+from django.shortcuts import render, redirect
+from news.forms import CategoryForm
+from news.models import News, Category
 
 # Create your views here.
 def home(request):
@@ -9,3 +10,16 @@ def home(request):
 def news_details(request, id):
     context = {"news": News.objects.get(id=id)}
     return render(request, 'news_details.html', context)
+
+def categories(request):
+    form = CategoryForm()
+
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            Category.objects.create(**form.cleaned_data)
+            return redirect("home-page")
+
+    context = {"form": form}
+    return render(request, 'categories_form.html', context)
